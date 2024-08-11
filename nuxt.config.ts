ddf3ad7ecf30fd063a992b50433e38f0e88a1110
medz <shiweidu@outlook.com>
2024-08-12 05:14:40 +0800

    fixed tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 import { defineNuxtConfig } from '@nuxt/bridge';
 
+import tailwind from "tailwindcss";
+
 export default defineNuxtConfig({
   bridge: {
     vite: true,
+  },
+  vite: {
+    css: {
+      postcss: [tailwind()],
+    }
   },
   modules: ['@nuxtjs/tailwindcss'],
 });
